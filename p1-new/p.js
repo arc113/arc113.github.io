2f6006c87e009fd2cbb301f13737a2253206694b
Archit <iarc13th@gmail.com>
2019-01-10 00:10:02 +0530

    Js functions

--- p1-new/p.js
+++ p1-new/p.js
@@ -7,12 +7,12 @@
 		  var l = 1366;
 		  var b = 1000;
 		
-		  h = Math.max(600, h);
+//		  h = Math.max(600, h);
 //		  w = Math.max(1300, w);
 
 		  
-      $('#left').append(h);
-      $('#left').append("<br>"+w);
+      $('#linner').append("<b>"+h+"x"+w+"</b>");
+//      $('#linner').append("<br>"+w);
 		
 		  var header = footer = (100/b)*h;
 		  var banner = (300/b)*h;
@@ -32,18 +32,18 @@
 		  var p_left_margin = -1*(60/l)*w;
 		  var m_top_margin = m_bottom_margin = (10/b)*h;
       var def_margin = (100/l)*w;                     //Side Margin
-      var ul_top_margin = (header - 20)/2;
+      var ul_top_margin = (header-header )/2;
       var li_margin = (50/l)*w;
-      
+      var temp = $("li").outerHeight();
+      var head2lip = 15*header/100;
 		  $("header").css("height", header+"px");
       $("#logo").css({"margin-top": logo_top+"px", "margin-left": def_margin+"px"});
-      $("#head2 ul").css({"top": ul_top_margin+"px",
-                                             "right": def_margin+"px",
-                                              "padding": "0px 0px 0px 0px"});
-      $("footer ul").css({"top": ul_top_margin+"px",
-                                          "padding": "0px 0px 0px 0px"});                                              
+      
+      $("#head2 li").css({"padding-top": head2lip+"px", "padding-bottom": head2lip+"px"});
+      $("footer li").css({"padding-top": head2lip+"px", "padding-bottom": head2lip+"px"});
+
       $("#banner").css({"height": banner+"px"});
-      $("h1").css({"left": (w-128)/2+"px", "top": h1_top+"px"}); 
+
       $("#arr_l").css({"top": arr_top+"px", "left": def_margin-10});
       $("#arr_r").css({"top": arr_top+"px", "right": def_margin-10});      
       $("#main").css({"margin-left": def_margin+"px", "margin-top": m_top_margin+"px", "width": main_x+"px", "height": main_y+"px"});
@@ -52,3 +52,30 @@
       $("#rupper").css({"height": orange_y+"px", "width": orange_x+"px"});
       $("#rlower").css({"height": green_y+"px", "width": green_x+"px", "margin-top": m_top_margin+"px"});
       $("footer").css({"height": footer+"px", "margin-top": m_top_margin+"px"});
+      
+      $(function () {
+          $(".arrow").click(function () {
+            var r = Math.floor(Math.random()*0);
+            var g = Math.floor(Math.random()*256);
+            var b = Math.floor(Math.random()*256);
+            var t = "rgb("+r+","+g+","+b+")";
+            var u = "rgb("+0+","+(127-g)+","+(127-b)+")";
+            $("#banner").css({"background-color":  t, "color": u});
+          });
+      });
+      
+      $(function () {
+          $("#head2 li").click(function () {
+            $("li").removeClass("selected");
+            $("li").css({"border-radius": "30px"});
+//             $("li").css({"background-color": "inherit"});
+//             $("li:hover").css({"background-color": "black"});
+//             $("li").css({"border-radius": "90px"});
+             
+            $(this).addClass("selected");
+            $(this).css({"border-radius": "0px"});
+          });
+      });
+
+
+
